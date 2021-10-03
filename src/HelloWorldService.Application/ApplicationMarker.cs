7c26c2d217905d9f1c1c2af26dc3cbe43a02ba95
ldeluigi <lucadelu97@gmail.com>
2021-10-03 14:35:59 +0200

    Rename solution and projects

---
+++ src/HelloWorldService.Application/ApplicationMarker.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloWorldService.Application
+{
+    /// <summary>
+    /// A marker type for the Application layer assembly.
+    /// </summary>
+    public sealed class ApplicationMarker
+    {
+    }
+}
