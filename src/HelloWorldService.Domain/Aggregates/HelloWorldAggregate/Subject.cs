9b58578bf2cbf5268747a5b704af9a9ba3594253
ldeluigi <lucadelu97@gmail.com>
2021-10-05 20:39:33 +0200

    Bug fix

--- src/HelloWorldService.Domain/Aggregates/HelloWorldAggregate/Subject.cs
+++ src/HelloWorldService.Domain/Aggregates/HelloWorldAggregate/Subject.cs
@@ -15,7 +15,7 @@ namespace HelloWorldService.Domain.Aggregates.HelloWorldAggregate
 
         protected override void Validate(string value)
         {
-            if (value.Length == 0)
+            if (value is null || value.Length == 0)
             {
                 throw new ArgumentException("Subject name can't be an empty string", nameof(value));
             }
